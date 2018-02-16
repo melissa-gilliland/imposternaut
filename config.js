7a082f03839d276e61e4de287ee1a6104c618498
melissa-gilliland <melissa.leigh.gilliland@gmail.com>
2018-02-16 18:24:55 -0500

    get started

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    consumer_key:         'EBBCnzMXSWcTBJDoUT5fTdJvw',
+    consumer_secret:      'RWhWjCqws0huglOfDLKve7XWR1pmk0wrHZCOiAYpVZm0JTdWwh',
+    access_token:         '964633841959952384-mtWRuhSKO0KvJwJL4DFjLWBY2olpzRN',
+    access_token_secret:  'h80gYIT16vZtiwE0iphryEK4febJdh5jqJ8HNVKt712eQ',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
