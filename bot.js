d6f67bfaf6b4c9b6a593674541578a17beed3f91
melissa-gilliland <melissa.leigh.gilliland@gmail.com>
2018-02-18 20:13:44 -0500

    add imposter tweets

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var config = require ('./config');
 
 var T = new Twit(config);
 
-var tweetArray = ['wow', 'hello', 'okay'];
+var tweetArray = ['Your self worth is not determined by your employment status.', 'No feedback does not equal negative feedback.', 'You\'re a person, not a project.', 'Your boss\'s opinions are not an indication of your value.', 'No one expect you to be perfect.', 'You are worth more than what you did or didn\'t accomplish today.', 'You have the right to ask for what you want.', 'It\'s okay to change your mind.', 'You don\'t have to be an expert to have a valid opinion.', 'You deserve to be treated with dignity and respect.', 'Your value is not determined by your productivity.', 'You don\'t have to be perfect.', 'You are not responsible for the behavior of others.', 'It\'s okay to say "I don\'t know."', 'It\'s okay if you didn\'t get everything done on your to-do list today. You did your best.', 'You don\'t have to get everything done right now. Maybe it\'t time to take a break? You can try again later.', 'It\'s okay if that thing didn\'t work out. You tried your best.', 'It\'s okay to take ownership of your schedule. You don\'t owe everyone your time.', 'You have the right to say no.', 'You don\'t have to be the best to be good.', 'You have the right to be angry, even if it\'t at a person you respect.', 'It\'s okay to feel afraid.', 'You have played a huge role in your many own successes.', 'Being wrong about something doesn\'t make you a fraud.', 'Nobody belongs here more than you.', 'It takes courage to be imperfect.', 'It\'s okay to feel confident while also acknowledging you have a lot to learn.';]
 
 var randomTweet = tweetArray[Math.floor(Math.random() * tweetArray.length)];
 
